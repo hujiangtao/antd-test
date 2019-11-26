901bca69d41afbb33177b6916f1683143d6e175e
Jonathan Whoo <jwhoo@localhost.localdomain>
2019-11-27 01:44:11 +0800

    before eject

--- src/pages/demo/life.js
+++ src/pages/demo/life.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Child from './child';
+import './index.less';
 
 export default class Life extends React.Component{
     constructor(props){
@@ -16,11 +17,7 @@ export default class Life extends React.Component{
     }
 
     render(){
-        let style = {
-            padding:50
-        };
-
-        return <div style={style}>
+        return <div className='content'>
             <p>React生命周期</p>
             <button onClick={this.handleAdd}>点击一下</button>
             <button onClick={this.handleClick.bind(this)}>点击一下</button>
