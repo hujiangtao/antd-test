dc19c6e6abf7bf443b9b0e5db0df23ca8fa6701a
Jonathan Whoo <jwhoo@localhost.localdomain>
2019-11-28 20:26:12 +0800

    样式表

--- src/admin.js
+++ src/admin.js
@@ -2,16 +2,20 @@ import React from 'react';
 import { Row, Col } from 'antd';
 import Header from './components/header';
 import Footer from './components/footer';
+import Nav from './components/nav';
+import './style/common.less';
 
 export default class Admin extends React.Component{
     render(){
         return(
-            <Row>
-                <Col span="3">Left</Col>
-                <Col span="21">
-                    <Header>Header</Header>
-                    <Row>content</Row>
-                    <Footer>Footer</Footer>
+            <Row className="container">
+                <Col span="3" className="nav-left">
+                    <Nav />
+                </Col>
+                <Col span="21" className="main">
+                    <Header />
+                    <Row className="content">content</Row>
+                    <Footer />
                 </Col>
             </Row>
         );
