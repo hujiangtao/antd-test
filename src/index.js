c4b30290d2248c44b6693e8068a8fb84b9671033
Jonathan Whoo <jwhoo@localhost.localdomain>
2019-12-07 19:54:28 +0800

    route练习1

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
-import Admin from './admin';
+// import Admin from './admin';
+import Home from './pages/route-demo/route1/home';
 
-ReactDOM.render(<Admin />, document.getElementById('root'));
+ReactDOM.render(<Home />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
